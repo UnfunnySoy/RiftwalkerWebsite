9676ae8327226122dceb7bfd4b253f50782a9148
Mitchell Myers <mmyersworking@gmail.com>
2025-10-05 12:18:28 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RiftwalkerWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
